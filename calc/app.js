c880d299780adf28e2958ab31ac92419457ab07f
Anya Aven <leafeon96@gmail.com>
2024-05-15 14:59:25 -0400

    /mean route functionality WIP

--- calc/app.js
+++ calc/app.js
@@ -1,16 +1,31 @@
 /** Simple demo Express app. */
-
+import { findMean, findMode, findMedian } from "./stats.js";
 import express from "express";
 const app = express();
 
 // useful error class to throw
-import { NotFoundError } from "./expressError.js";
+import { NotFoundError, BadRequestError } from "./expressError.js";
 
-const MISSING = "Expected key `nums` with comma-separated list of numbers.";
+const MISSING_ERR_MSG = "Expected key `nums` with comma-separated list of numbers.";
 
 
 /** Finds mean of nums in qs: returns {operation: "mean", result } */
 
+app.get("/mean", function (req, res) {
+  if (!("nums" in req.query)) throw new BadRequestError(MISSING_ERR_MSG);
+
+
+  // "?nums=1,2,3,4"
+  const nums = req.query.nums.split(",");
+  const notNums = nums.filter(n => isNaN(n));
+  if (notNums.length !== 0) {
+    throw new BadRequestError(`${notNums} <-- these are not nums`);
+  }
+
+  const value = findMean(nums);
+
+  return res.json({ operation: "mean", value });
+});
 
 /** Finds median of nums in qs: returns {operation: "median", result } */
 
