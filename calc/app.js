82ee15f1d9b57d1b50c4f65bb6bb851e2c2a4b35
Anya Aven <leafeon96@gmail.com>
2024-05-15 16:21:28 -0400

    Refactored to have all operation routes in a single route

--- calc/app.js
+++ calc/app.js
@@ -13,58 +13,97 @@ app.use(express.json());                           // process JSON data
 app.use(express.urlencoded());                     // process trad form data
 
 
-/** Finds mean of nums in qs: returns {operation: "mean", result } */
-app.get("/mean", function (req, res) {
+// /** Finds mean of nums in qs: returns {operation: "mean", result } */
+// app.get("/mean", function (req, res) {
+//   if (!("nums" in req.query)) throw new BadRequestError(MISSING_ERR_MSG);
+
+//   const nums = req.query.nums.split(",");
+//   const convertedNums = convertStrNums(nums);
+
+//   const result = findMean(convertedNums);
+
+//   return res.json({ operation: "mean", result });
+// });
+
+// /** Finds median of nums in qs: returns {operation: "median", result } */
+// app.get("/median", function (req, res) {
+//   if (!("nums" in req.query)) throw new BadRequestError(MISSING_ERR_MSG);
+
+//   const nums = req.query.nums.split(",");
+//   const convertedNums = convertStrNums(nums);
+
+//   const result = findMedian(convertedNums);
+
+//   return res.json({ operation: "median", result });
+// });
+
+// /** Finds mode of nums in qs: returns {operation: "mean", result } */
+// app.get("/mode", function (req, res) {
+//   if (!("nums" in req.query)) throw new BadRequestError(MISSING_ERR_MSG);
+
+//   const nums = req.query.nums.split(",");
+//   const convertedNums = convertStrNums(nums);
+
+//   const result = findMode(convertedNums);
+
+//   return res.json({ operation: "mode", result });
+// });
+
+app.get("/all", function (req, res) {
   if (!("nums" in req.query)) throw new BadRequestError(MISSING_ERR_MSG);
 
   const nums = req.query.nums.split(",");
   const convertedNums = convertStrNums(nums);
 
-  const result = findMean(convertedNums);
-
-  return res.json({ operation: "mean", result });
-});
-
-/** Finds median of nums in qs: returns {operation: "median", result } */
-app.get("/median", function (req, res) {
-  if (!("nums" in req.query)) throw new BadRequestError(MISSING_ERR_MSG);
-
-  const nums = req.query.nums.split(",");
-  const convertedNums = convertStrNums(nums);
-
-  const result = findMedian(convertedNums);
-
-  return res.json({ operation: "median", result });
-});
-
-/** Finds mode of nums in qs: returns {operation: "mean", result } */
-app.get("/mode", function (req, res) {
-  if (!("nums" in req.query)) throw new BadRequestError(MISSING_ERR_MSG);
-
-  const nums = req.query.nums.split(",");
-  const convertedNums = convertStrNums(nums);
-
-  const result = findMode(convertedNums);
-
-  return res.json({ operation: "mode", result });
-});
-
-app.get("/all", function (req, res){
-  if (!("nums" in req.query)) throw new BadRequestError(MISSING_ERR_MSG);
-  
-  const nums = req.query.nums.split(",");
-  const convertedNums = convertStrNums(nums);
-  
   const mean = findMean(convertedNums);
   const median = findMedian(convertedNums);
   const mode = findMode(convertedNums);
-  
-  return res.json({response: {
-    operation: "all",
-    mean,
-    median,
-    mode,
-  }})
+
+  return res.json({
+    response: {
+      operation: "all",
+      mean,
+      median,
+      mode,
+    }
+  });
+});
+
+/** Finds mean, mode, or median of nums in qs:
+ *
+ * Return example if <operation> is mean:
+  {operation: "mean", result } */
+app.get("/:operation", function (req, res) {
+  if (!("nums" in req.query)) throw new BadRequestError(MISSING_ERR_MSG);
+  const operation = req.params.operation;
+
+  const nums = req.query.nums.split(",");
+  const convertedNums = convertStrNums(nums);
+
+  let result;
+  switch (operation) {
+    case "mean":
+      result = findMean(convertedNums);
+      break;
+
+    case "median":
+      result = findMedian(convertedNums);
+      break;
+
+    case "mode":
+      result = findMode(convertedNums);
+      break;
+
+    default:
+      result = "Operation not found"
+  }
+
+  return res.json({
+    response: {
+      operation,
+      result,
+    }
+  });
 });
 
 /** 404 handler: matches unmatched routes; raises NotFoundError. */
