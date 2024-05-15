1a8b5772091444c3c6e7565ee17329a49ad6c44a
Jacob <jacobrashid404@gmail.com>
2024-05-15 12:28:04 -0700

    mean, median, mode, all routes finished

--- calc/app.js
+++ calc/app.js
@@ -1,5 +1,6 @@
 /** Simple demo Express app. */
 import { findMean, findMode, findMedian } from "./stats.js";
+import { convertStrNums } from "./utils.js";
 import express from "express";
 const app = express();
 
@@ -8,30 +9,63 @@ import { NotFoundError, BadRequestError } from "./expressError.js";
 
 const MISSING_ERR_MSG = "Expected key `nums` with comma-separated list of numbers.";
 
+app.use(express.json());                           // process JSON data
+app.use(express.urlencoded());                     // process trad form data
+
 
 /** Finds mean of nums in qs: returns {operation: "mean", result } */
-
 app.get("/mean", function (req, res) {
   if (!("nums" in req.query)) throw new BadRequestError(MISSING_ERR_MSG);
 
-
-  // "?nums=1,2,3,4"
   const nums = req.query.nums.split(",");
-  const notNums = nums.filter(n => isNaN(n));
-  if (notNums.length !== 0) {
-    throw new BadRequestError(`${notNums} <-- these are not nums`);
-  }
+  const convertedNums = convertStrNums(nums);
 
-  const value = findMean(nums);
+  const result = findMean(convertedNums);
 
-  return res.json({ operation: "mean", value });
+  return res.json({ operation: "mean", result });
 });
 
 /** Finds median of nums in qs: returns {operation: "median", result } */
+app.get("/median", function (req, res) {
+  if (!("nums" in req.query)) throw new BadRequestError(MISSING_ERR_MSG);
 
+  const nums = req.query.nums.split(",");
+  const convertedNums = convertStrNums(nums);
+
+  const result = findMedian(convertedNums);
+
+  return res.json({ operation: "median", result });
+});
 
 /** Finds mode of nums in qs: returns {operation: "mean", result } */
+app.get("/mode", function (req, res) {
+  if (!("nums" in req.query)) throw new BadRequestError(MISSING_ERR_MSG);
 
+  const nums = req.query.nums.split(",");
+  const convertedNums = convertStrNums(nums);
+
+  const result = findMode(convertedNums);
+
+  return res.json({ operation: "mode", result });
+});
+
+app.get("/all", function (req, res){
+  if (!("nums" in req.query)) throw new BadRequestError(MISSING_ERR_MSG);
+  
+  const nums = req.query.nums.split(",");
+  const convertedNums = convertStrNums(nums);
+  
+  const mean = findMean(convertedNums);
+  const median = findMedian(convertedNums);
+  const mode = findMode(convertedNums);
+  
+  return res.json({response: {
+    operation: "all",
+    mean,
+    median,
+    mode,
+  }})
+});
 
 /** 404 handler: matches unmatched routes; raises NotFoundError. */
 app.use(function (req, res) {
